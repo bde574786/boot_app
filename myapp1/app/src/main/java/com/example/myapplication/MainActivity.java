f44e16caad5ed7f437be40c27e9bc2a342c096d5
bde574786 <bde574786@naver.com>
2022-05-23 20:39:50 +0900

    계산기 만들기

--- myapp1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ myapp1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v4.os.IResultReceiver;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "TGA";
+    private static final String TAG = "TAG";
     private TextView zero;
     private TextView one;
     private TextView two;
@@ -29,10 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView divide;
     private TextView equal;
 
-
-    String oldvalue = "0";
+    ArrayList<String> arrListFormularValues;
     String newvalue = "0";
 
+    String formula = "";
+    boolean operatorOnOff = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initData() {
-//        r = 주소값
         zero = findViewById(R.id.zero);
         one = findViewById(R.id.one);
         two = findViewById(R.id.two);
@@ -57,96 +62,228 @@ public class MainActivity extends AppCompatActivity {
         nine = findViewById(R.id.nine);
         ca = findViewById(R.id.ca);
         plus = findViewById(R.id.plus);
-        multiply= findViewById(R.id.multiply);
+        multiply = findViewById(R.id.multiply);
         substaract = findViewById(R.id.substract);
         divide = findViewById(R.id.divide);
         equal = findViewById(R.id.equal);
         result = findViewById(R.id.result);
+        arrListFormularValues = new ArrayList<>();
+    }
 
+    private void setResultTextView(String number) {
+        if (newvalue.equals("0")) {
+            result.setText(number);
+            newvalue = "";
+        }
+        newvalue += number;
+        result.setText(newvalue);
+        operatorOnOff = false;
+    }
 
+    private void setOperatorToFormula(String operator) {
 
-        System.out.println("initData 메서드 호출");
+        if (!operatorOnOff) {
+            formula += operator;
+        }
+
+        operatorOnOff = false;
+
     }
 
 
-    private void addEventListener() {
-        zero.setOnClickListener(view -> {
-            newvalue = newvalue + "0";
-            System.out.println(newvalue);
-            System.out.println(newvalue.substring(0, 1));
-            if(newvalue.substring(0,1).equals("0")){
-                if(newvalue.length()>0){
-                    newvalue = newvalue.substring(1, newvalue.length()-1);
-                }
+    private ArrayList<String> removeStartEndOperArrList(ArrayList<String> arrListFormularValues) {
 
+        int arrListSize = arrListFormularValues.size();
+        if (arrListFormularValues.get(arrListSize - 1).equals("+") |
+                arrListFormularValues.get(arrListSize - 1).equals("-") |
+                arrListFormularValues.get(arrListSize - 1).equals("*") |
+                arrListFormularValues.get(arrListSize - 1).equals("/")) {
+            arrListFormularValues.remove(arrListSize - 1);
+        }
+        return arrListFormularValues;
+    }
+
+    private int findIndex(ArrayList<String> arrListFormularValues) {
+        int idx1;
+        int idx2;
+        int opIndex = 0;
+        if ((arrListFormularValues.indexOf("*") != -1) || (arrListFormularValues.indexOf("/") != -1)) {
+            idx1 = arrListFormularValues.indexOf("*");
+            idx2 = arrListFormularValues.indexOf("/");
+            if (idx1 == -1 && idx2 != -1) {
+                opIndex = idx2;
+            } else if (idx2 == -1 && idx1 != -1) {
+                opIndex = idx1;
+
+            } else {
+                opIndex = (idx1 < idx2) ? idx1 : idx2;
             }
-            result.setText(newvalue);
-        });
+        } else if ((arrListFormularValues.indexOf("+") != -1) || (arrListFormularValues.indexOf("-") != -1)) {
+            idx1 = arrListFormularValues.indexOf("+");
+            idx2 = arrListFormularValues.indexOf("-");
+
+            if (idx1 == -1 && idx2 != -1) {
+                opIndex = idx2;
+            } else if (idx2 == -1 && idx1 != -1) {
+                opIndex = idx1;
+            } else {
+                opIndex = (idx1 < idx2) ? idx1 : idx2;
+            }
+        }
+
+        return opIndex;
+    }
+
+
+
+    private ArrayList<String> subCalaulate(int opIndex) {
+        double leftNumber = Double.parseDouble(arrListFormularValues.get(opIndex - 1));
+        double rightNumber = Double.parseDouble(arrListFormularValues.get(opIndex + 1));
+        double result = 0.0;
+
+        String operator = arrListFormularValues.get(opIndex);
+
+        switch (operator) {
+            case "*":
+                result = leftNumber * rightNumber;
+                break;
+            case "/":
+                result = leftNumber / rightNumber;
+                break;
+            case "+":
+                result = leftNumber + rightNumber;
+                break;
+            case "-":
+                result = leftNumber - rightNumber;
+                break;
+
+        }
+
+
+        arrListFormularValues.set(opIndex - 1, String.valueOf(result));
+        arrListFormularValues.remove(opIndex);
+        arrListFormularValues.remove(opIndex);
+
+
+
+        return arrListFormularValues;
+    }
+
+    private ArrayList<String> operateByPriority(ArrayList<String> arrListFormularValues) {
+        while (arrListFormularValues.size() > 1) {
+            arrListFormularValues = subCalaulate(findIndex(arrListFormularValues));
+        }
+        return arrListFormularValues;
+    }
+
+    private void addEventListener() {
 
         one.setOnClickListener(view -> {
-            newvalue = newvalue + "1";
-            result.setText(newvalue);
+            formula += "1";
+            setResultTextView("1");
         });
 
+
         two.setOnClickListener(view -> {
-            newvalue = newvalue + "2";
-            result.setText(newvalue);
+            formula += "2";
+            setResultTextView("2");
         });
 
         three.setOnClickListener(view -> {
-            newvalue = newvalue + "3";
-            result.setText(newvalue);
+            formula += "3";
+            setResultTextView("3");
         });
+
         four.setOnClickListener(view -> {
-            newvalue = newvalue + "4";
-            result.setText(newvalue);
+            formula += "4";
+            setResultTextView("4");
         });
+
         five.setOnClickListener(view -> {
-            newvalue = newvalue + "5";
-            result.setText(newvalue);
+            formula += "5";
+            setResultTextView("5");
         });
+
         six.setOnClickListener(view -> {
-            newvalue = newvalue + "6";
-            result.setText(newvalue);
+            formula += "6";
+            setResultTextView("6");
         });
+
         seven.setOnClickListener(view -> {
-            newvalue = newvalue + "7";
-            result.setText(newvalue);
+            formula += "7";
+            setResultTextView("7");
         });
+
         eight.setOnClickListener(view -> {
-            newvalue = newvalue + "8";
-            result.setText(newvalue);
+            formula += "8";
+            setResultTextView("8");
         });
+
         nine.setOnClickListener(view -> {
-            newvalue = newvalue + "9";
-            result.setText(newvalue);
+            formula += "9";
+            setResultTextView("9");
         });
+
+        zero.setOnClickListener(view -> {
+            formula += "0";
+            setResultTextView("0");
+        });
+
+
         ca.setOnClickListener(view -> {
-            newvalue = "";
+            arrListFormularValues.clear();
+            newvalue = "0";
+            formula = "";
             result.setText("0");
         });
 
+
         plus.setOnClickListener(view -> {
+            String strTemp = String.valueOf(result.getText());
+            arrListFormularValues.add(strTemp);
+            setOperatorToFormula("+");
+            operatorOnOff = true;
+            arrListFormularValues.add("+");
+            newvalue = "0";
         });
 
         multiply.setOnClickListener(view -> {
-            Log.d(TAG, "*");
+            String strTemp = String.valueOf(result.getText());
+            arrListFormularValues.add(strTemp);
+            setOperatorToFormula("*");
+            operatorOnOff = true;
+            arrListFormularValues.add("*");
+            newvalue = "0";
         });
 
         substaract.setOnClickListener(view -> {
-            Log.d(TAG, "-");
+            String strTemp = String.valueOf(result.getText());
+            arrListFormularValues.add(strTemp);
+            setOperatorToFormula("-");
+            operatorOnOff = true;
+            arrListFormularValues.add("-");
+            newvalue = "0";
         });
 
         divide.setOnClickListener(view -> {
-            Log.d(TAG, "%");
+            String strTemp = String.valueOf(result.getText());
+            arrListFormularValues.add(strTemp);
+            setOperatorToFormula("/");
+            operatorOnOff = true;
+            arrListFormularValues.add("/");
+            newvalue = "0";
         });
 
         equal.setOnClickListener(view -> {
-
-
-
-            Log.d(TAG, "=");
+            String strTemp = String.valueOf(result.getText());
+            arrListFormularValues.add(strTemp);
+            arrListFormularValues = removeStartEndOperArrList(arrListFormularValues);
+            arrListFormularValues = operateByPriority(arrListFormularValues);
+            result.setText(arrListFormularValues.get(0));
+            arrListFormularValues.clear();
         });
+
+
     }
 
 }
